2bc27eaf40dd05415da71e6b6bfe80f44ddda915
Darshan Regmi <regmidarshan545@gmail.com>
2025-01-31 22:39:28 +0545

    formatted document and final courswork

--- assets/js/cartFunction.js
+++ assets/js/cartFunction.js
@@ -1,16 +1,16 @@
 // Add to Cart Functionality
 function addToCart(productId, productName, productPrice) {
-    // Get existing cart data from localStorage
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
-    
-    // Add new product to the cart
-    cart.push({
-        id: productId,
-        name: productName,
-        price: productPrice
-    });
+  // Get existing cart data from localStorage
+  let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-    // Save updated cart back to localStorage
-    localStorage.setItem('cart', JSON.stringify(cart));
-    alert(`${productName} has been added to the cart!`);
+  // Add new product to the cart
+  cart.push({
+    id: productId,
+    name: productName,
+    price: productPrice,
+  });
+
+  // Save updated cart back to localStorage
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert(`${productName} has been added to the cart!`);
 }
